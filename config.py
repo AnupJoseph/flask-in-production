561e45d3caddb0e8bda30baa68701a5d28e434b0
AnupJoseph <anup20joseph@mail.com>
2020-08-08 12:29:38 +0530

    Completed database Building

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ class Config(object):
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = "no-one-will-figure-this-out"
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 class ProductionConfig(Config):
     DEBUG = False
